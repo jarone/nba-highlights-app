e8aee953c4d983aed261c5515bc098bbf4e64685
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2016-02-16 23:46:22 +0100

    basic components structure

---
+++ app/both/components/MainLayout.import.jsx
@@ -0,0 +1,15 @@
+export default React.createClass({
+	render() {
+		return <div>
+			<header>
+
+			</header>
+			<main>
+				{this.props.content}
+			</main>
+			<footer>
+
+			</footer>
+		</div>
+	}
+});
