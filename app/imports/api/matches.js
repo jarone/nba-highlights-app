bb37a6f9e030ad31309c46d49051d73b03686a16
paufabregat <pau.fabregat.p@gmail.com>
2016-09-18 11:14:05 +0000

    publish collection

--- app/imports/api/matches.js
+++ app/imports/api/matches.js
@@ -1,3 +1,11 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 
 export const Matches = new Mongo.Collection('matches');
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('matches', function matchesPublication() {
+    return Matches.find();
+  });
+}
