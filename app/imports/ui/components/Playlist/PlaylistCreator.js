1541b11ea1c515e1499220a0e3a246cf44326f43
paufabregat <pau.fabregat.p@gmail.com>
2016-10-02 13:49:12 +0000

    Add component to create playlist

---
+++ app/imports/ui/components/Playlist/PlaylistCreator.js
@@ -0,0 +1,44 @@
+import { Row, Col, Button } from 'react-bootstrap';
+import React, { Component, PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import _ from 'lodash';
+
+
+export class PlaylistCreator extends Component {
+	constructor(props) {
+    super(props);
+
+    this.state = {
+      // displayPlaylist: false
+    };
+  }
+
+	createPlaylist() {
+		console.log(this.props.videos);
+	}
+
+  render() {
+    let content = null;
+  	if(!_.isEmpty(this.props.videos)) {
+  		content = (
+  			<Row className='create-playlist'>
+  	      <Col xs={12}>
+            <div>
+    	       <Button
+    	          bsSize='large'
+    	          onClick={this.createPlaylist.bind(this)}
+    	        >
+    	          Create Playlist!
+    	        </Button>
+            </div>
+  	      </Col>
+  	    </Row>
+  		);
+  	}
+    return (
+      <div>
+          {content}
+      </div>
+    )
+  }
+}
